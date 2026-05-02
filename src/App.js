fa55f189c33bf4d2b3a7d64906c8cc976ef4b19b
Fatou Mbaye <fatoumbaye1@esp.sn>
2026-05-02 14:21:39 +0200

    statistique des lignes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
+import Statistique from './Statistique';
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
 
       <main>
         <p>Bienvenue sur SénTransport</p>
+        <Statistique />
+        <Statistique />
+        <Statistique />
       </main>
 
       <Footer />
---
+++ src/Statistique.js
@@ -0,0 +1,12 @@
+
+
+function Statistique() {
+  return (
+    <div className="stat">
+      <h2>10</h2>
+      <p>lignes</p>
+    </div>
+  );
+}
+
+export default Statistique;
